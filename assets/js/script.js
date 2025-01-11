7b3797eba53c7dd82a509a67f26f2cf3f226a6d8
Chris Wendell Flaviano <112796993+CWF105@users.noreply.github.com>
2025-01-12 01:02:59 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.navs');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('toggle');
+});
+
+function openModal(title) {
+    switch(title) {
+        case "julian":
+            document.getElementById('myModal1').style.display = 'flex';
+            break;
+        case "AR":
+            document.getElementById('myModal2').style.display = 'flex';
+            break;
+    }
+}
+
+function closeModal(title) {
+    switch(title) {
+        case "julian":
+            document.getElementById('myModal1').style.display = 'none';
+            break;
+        case "AR":
+            document.getElementById('myModal2').style.display = 'none';
+            break;
+    }
+}
+
